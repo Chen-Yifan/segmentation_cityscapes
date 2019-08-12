9f6bdc11061936f926948495b43afd2603934a7f
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-08-12 14:49:30 -0500

    all

--- train.py
+++ train.py
@@ -16,11 +16,11 @@ import argparse
 def get_callbacks(name_weights, path, patience_lr):
     mcp_save = ModelCheckpoint(name_weights, save_best_only=False, monitor='iou_score', mode='max')
 #     reduce_lr_loss = ReduceLROnPlateau(monitor='bce_jaccard_loss', factor=0.5, patience=patience_lr, verbose=1, epsilon=1e-4, mode='min')
-    reduce_lr_loss = ReduceLROnPlateau(factor=0.5)
+#    reduce_lr_loss = ReduceLROnPlateau(factor=0.5)
     logdir = os.path.join(path,'log')
     tensorboard = TensorBoard(log_dir=logdir, histogram_freq=0,
                             write_graph=True, write_images=True)
-    return [mcp_save, reduce_lr_loss, tensorboard]
+    return [mcp_save, tensorboard]
 
 #get arguments
 parser = argparse.ArgumentParser()
@@ -29,6 +29,7 @@ parser.add_argument("--ckpt_path", type=str, default='/media/exfat/yifan/rf_chec
 parser.add_argument("--results_path", type=str, default='/media/exfat/yifan/rf_results/cityscapes_unet_100e/')
 parser.add_argument("--batch_size", type=int, default=16)
 parser.add_argument("--epochs", type=int, default=100)
+parser.add_argument("--opt", type=int, default=1)
 
 args = parser.parse_args()
 
@@ -61,9 +62,13 @@ m = Unet(classes = 20, input_shape=(256, 256, 3), activation='softmax')
 m.summary()
 
 #optimizer
-opt= Adam(lr = 1e-4)
-opt2 = SGD(lr=0.01, decay=1e-6, momentum=0.99, nesterov=True)
-opt3 = Adadelta(lr=1, rho=0.95, epsilon=1e-08, decay=0.0)
+
+if args.opt==1:
+    opt= Adam(lr = 1e-4)
+elif args.opt==2:
+    opt = SGD(lr=0.01, decay=1e-6, momentum=0.99, nesterov=True)
+else:
+    opt = Adadelta(lr=1, rho=0.95, epsilon=1e-08, decay=0.0)
 m.compile(optimizer=opt, loss='categorical_crossentropy', metrics=[iou_score])
 
 # fit model
--- train_ncsa.sh
+++ train_ncsa.sh
@@ -1,5 +1,6 @@
 python train.py \
 --dataset_path /home/yifanc3/Github/segmentation_train/dataset/cityscapes_256/ \
---ckpt_path /home/yifanc3/models/rf_checkpoints/cityscapes_unet_softmax_AdamRd_100e/ \
---results_path /home/yifanc3/models/rf_results/cityscapes_unet_softmax_AdamRd_100e/ \
---epochs 100
+--ckpt_path /home/yifanc3/models/rf_checkpoints/cityscapes_unet_softmax_Adal_100e/ \
+--results_path /home/yifanc3/models/rf_results/cityscapes_unet_softmax_Adal_100e/ \
+--epochs 100 \
+--opt 3
