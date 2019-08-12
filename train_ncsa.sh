f062886a09b3529e2a2bf0129f021bdc83840524
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-08-12 12:54:41 -0500

    all

--- train_ncsa.sh
+++
@@ -1,4 +0,0 @@
-python train.py \
---ckpt_path /home/yifanc3/models/rf_checkpoints/cityscapes_unet_softmax_Adal_100e/ \
---results_path /home/yifanc3/models/rf_results/cityscapes_unet_softmax_Adal_100e/ \
---epochs 100
