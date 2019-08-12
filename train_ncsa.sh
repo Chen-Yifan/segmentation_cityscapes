62111295b44a2e06b65a6576e538b8eb6a02c369
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-08-12 00:37:40 -0500

    train.pbs nano

--- train_ncsa.sh
+++ train_ncsa.sh
@@ -1,4 +1,5 @@
 python train.py \
---ckpt_path /home/yifanc3/models/rf_checkpoints/cityscapes_unet_softmax_Adal_100e/ \
---results_path /home/yifanc3/models/rf_results/cityscapes_unet_softmax_Adal_100e/ \
+--dataset_path /home/yifanc3/Github/segmentation_train/dataset/cityscapes_256/ \
+--ckpt_path /home/yifanc3/models/rf_checkpoints/cityscapes_unet_softmax_AdamRd_100e/ \
+--results_path /home/yifanc3/models/rf_results/cityscapes_unet_softmax_AdamRd_100e/ \
 --epochs 100
