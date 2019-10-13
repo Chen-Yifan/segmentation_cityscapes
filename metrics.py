mean_iou correct, mind that y_true has (batch,w,h,1) which needs to be sqeezed in loss and metric calculation

metrics.py
@@ -4,12 +4,15 @@ import os
 import glob
 import skimage.io as io
 import tensorflow as tf
+from segmentation_models.metrics import iou_score
+labels = __import__('labels')
+Id2ignore = {label.id: label.ignoreInEval for label in labels.labels} 
 
 def Mean_IOU(y_true, y_pred):
-    nb_classes = K.int_shape(y_pred)[-1] #20
+    nb_classes = K.int_shape(y_pred)[-1] 
     print(nb_classes)
     iou = []
-    true_pixels = K.argmax(y_true, axis=-1) # = (n, h,w)
+    true_pixels = K.squeeze(y_true,axis=-1)  # = (n, h,w)
     pred_pixels = K.argmax(y_pred, axis=-1) 
     #ignore certain labels, those doesn't have one, and those are background
     void_labels = K.equal(true_pixels, 19) # ignore label 19, background
@@ -27,3 +30,43 @@ def Mean_IOU(y_true, y_pred):
     legal_labels = ~tf.debugging.is_nan(iou)
     iou = tf.gather(iou, indices=tf.where(legal_labels))
     return K.mean(iou)
+
+def meaniou(y_true, y_pred):
+    '''
+    In this function, we calculate the meaniou between labels and predictions 
+    y_true is in (batch, h, w, 1), we need to cast it into 'int8' and reshape to (batch, h, w, cl)
+    after the transformation, we calculate the meaniou
+    '''
+    y_true = K.squeeze(y_true,axis=-1)  
+    y_true = K.cast(y_true, 'int32')
+    cl = K.int_shape(y_pred)[-1] 
+    y_true = K.one_hot( y_true, num_classes=cl)
+    return iou_score(y_true, y_pred)
+
+def meaniou_partial(y_true, y_pred):
+    '''this function maintains most of the functionality in meaniou,
+    Only that we ignore those ignoreInEval classess listed in labels
+    '''
+    y_true = K.squeeze(y_true,axis=-1)  
+    y_true = K.cast(y_true, 'int32')
+    shape = K.int_shape(y_pred)
+    cl = shape[-1] 
+    y_true = K.one_hot( y_true, num_classes=cl)
+    
+    
+    n = 0
+    id_list = []
+    for i in range(cl): #0...33
+        if(Id2ignore[i] == False): # not ignore
+            n += 1
+            id_list.append(i)
+    print('n is' ,n)
+    
+#     y_pred = np.array(y_pred)
+#     y_true = np.array(y_true)
+#     print(y_pred.shape, y_true.shape)
+#     y_pred_clean = np.concatenate([y_pred[:,:,:,a] for a in id_list], axis=-1)
+#     y_true_clean = np.concatenate([y_true[:,:,:,a] for a in id_list], axis=-1)
+    
+    return iou_score(y_true_clean, y_pred_clean) 
+            

train.py
@@ -1,7 +1,6 @@
 from segmentation_models import Unet
 # from segmentation_models.backbones import get_preprocessing
 from segmentation_models.losses import bce_jaccard_loss
-from segmentation_models.metrics import iou_score
 from keras.optimizers import SGD,Adam,Adadelta
 from keras.callbacks import TensorBoard
 from keras.callbacks import ModelCheckpoint
@@ -66,7 +65,7 @@ gpus = args.gpus
 '''
 input_shape = (h,w,3)
 if (args.network == 'Unet'):
-    m = Unet(classes = cl, input_shape=input_shape, activation=None)
+    m = Unet(classes = cl, input_shape=input_shape, activation='softmax')
 #     m = get_unet()
 elif (args.network == 'unet_noskip'):
     m = unet_noskip()
@@ -89,19 +88,18 @@ if(gpus>1):
         m = multi_gpu_model(m, gpus=gpus)
         print("Training using multiple GPUs..")
     except:
-        print("Training using single GPU or CPU..")    
+        print("Training using single GPU or CPU..")
 
-# def sparse_softmax_cce(y_true, y_pred):
-#     y_true = y_true[:,:,:,0]
-#     y_true = tf.cast(y_true, 'int32')
-#     print(y_true.get_shape())
-#     return tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred)
+def sparse_softmax_cce(y_true, y_pred):
+    y_true = K.squeeze(y_true, axis=-1)
+    y_true = tf.cast(y_true, 'int32')
+    print(y_true.get_shape())
+    return tf.keras.backend.sparse_categorical_crossentropy(y_true,y_pred)
 
-m.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=[iou_score])
-
+m.compile(optimizer=opt, loss=sparse_softmax_cce, metrics=[meaniou])
 
 # fit model
-weights_path = args.ckpt_path + 'weights.{epoch:02d}-{val_loss:.2f}-{val_iou_score:.2f}.hdf5'
+weights_path = args.ckpt_path + 'weights.{epoch:02d}-{val_loss:.2f}-{val_meaniou:.2f}.hdf5'
 callbacks = get_callbacks(weights_path, args.ckpt_path, 5, args.opt)
 
 train_generator,num_train = dataGen(frame_path, mask_path, BATCH_SIZE, args.epochs, (h,w))